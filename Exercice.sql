1956cdf398815f2585c582c2abe6a07e53a5f538
yanbert <yanbert@gmail.com>
2023-02-18 17:09:46 +0000

    mes commentaires dans le code

--- Exercice.sql
+++ Exercice.sql
@@ -12,6 +12,7 @@ VALUES('Matt','Damon','08/10/1970', 5);
 INSERT INTO actors (first_name, last_name, age, number_oscars)
 VALUES('George','Clooney','06/05/1961', 2);
 
+--- cette requete ne marchera pas
 SELECT COUNT (*) FROM actors;
 
 __ On peut pas entrer un champs vide car lors de la création de notre table on a spécifié non null
